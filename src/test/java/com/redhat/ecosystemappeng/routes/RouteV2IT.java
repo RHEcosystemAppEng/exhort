cd7c7b9187620cd05ece69628a8b4a9323431ae2
ruromero <rromerom@redhat.com>
2023-03-30 17:13:06 +0200

    Add Integration tests and CI
    Signed-off-by: ruromero <rromerom@redhat.com>

---
+++ src/test/java/com/redhat/ecosystemappeng/routes/RouteV2IT.java
@@ -0,0 +1,10 @@
+package com.redhat.ecosystemappeng.routes;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class RouteV2IT extends RouteV2Test {
+    
+    // Run the same tests
+    
+}
---
+++ src/test/java/com/redhat/ecosystemappeng/routes/RouteV3IT.java
@@ -0,0 +1,10 @@
+package com.redhat.ecosystemappeng.routes;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class RouteV3IT extends RouteV3Test {
+    
+    // Run the same tests
+
+}
