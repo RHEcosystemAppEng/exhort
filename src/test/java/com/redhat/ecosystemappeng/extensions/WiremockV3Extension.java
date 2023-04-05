bf995c6de47d194537d39457803c6d169221f0c7
Ruben Romero Montes <rromerom@redhat.com>
2023-04-05 13:57:18 +0200

    Allow use client snyk token (#11)
    Signed-off-by: ruromero <rromerom@redhat.com>

--- src/test/java/com/redhat/ecosystemappeng/extensions/WiremockV3Extension.java
+++ src/test/java/com/redhat/ecosystemappeng/extensions/WiremockV3Extension.java
@@ -1,15 +1,8 @@
 package com.redhat.ecosystemappeng.extensions;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 import java.util.Map;
-
-import javax.ws.rs.core.MediaType;
-
-import org.apache.camel.Exchange;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 
@@ -17,27 +10,13 @@ import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 
 public class WiremockV3Extension implements QuarkusTestResourceLifecycleManager {
 
-    private static final String SNYK_TOKEN = "snyk-token-xyz";
+    public static final String SNYK_TOKEN = "snyk-token-xyz";
 
     private final WireMockServer server = new WireMockServer(options().dynamicPort());
 
     @Override
     public Map<String, String> start() {
         server.start();
-
-        server.stubFor(post("/api/v1/test/dep-graph")
-                .withHeader("Authorization", equalTo("token " + SNYK_TOKEN))
-                .withHeader(Exchange.CONTENT_TYPE, equalTo(MediaType.APPLICATION_JSON))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withBodyFile("snyk_report.json")));
-
-        server.stubFor(post("/api/policy/v1alpha1/trusted::gav?minimal=true")
-                .withHeader(Exchange.CONTENT_TYPE, equalTo(MediaType.APPLICATION_JSON))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader(Exchange.CONTENT_ENCODING, "identity")
-                        .withBodyFile("trustedContent_report.json")));
 
         return Map.of(
                 "api.snyk.host", server.baseUrl(),
@@ -52,4 +31,8 @@ public class WiremockV3Extension implements QuarkusTestResourceLifecycleManager 
         }
     }
 
+    @Override
+    public void inject(TestInjector testInjector) {
+        testInjector.injectIntoFields(server, new TestInjector.AnnotatedAndMatchesType(InjectWireMock.class, WireMockServer.class));
+    }
 }
--- src/test/java/com/redhat/ecosystemappeng/routes/DependencyAnalyticsTest.java
+++ src/test/java/com/redhat/ecosystemappeng/routes/DependencyAnalyticsTest.java
@@ -1,5 +1,10 @@
 package com.redhat.ecosystemappeng.routes;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static io.restassured.RestAssured.given;
 import static org.apache.camel.Exchange.CONTENT_TYPE;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -12,17 +17,17 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.core.MediaType;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.camel.Exchange;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.redhat.ecosystemappeng.config.ObjectMapperProducer;
+import com.redhat.ecosystemappeng.extensions.InjectWireMock;
 import com.redhat.ecosystemappeng.extensions.WiremockV3Extension;
 import com.redhat.ecosystemappeng.model.ComponentRequest;
 import com.redhat.ecosystemappeng.model.PackageRef;
@@ -38,8 +43,16 @@ import io.restassured.path.xml.XmlPath.CompatibilityMode;
 @QuarkusTestResource(WiremockV3Extension.class)
 public class DependencyAnalyticsTest {
 
+    @InjectWireMock
+    WireMockServer server;
+
     static {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @AfterEach
+    void resetMock() {
+        server.resetAll();
     }
 
     @Test
@@ -55,6 +68,8 @@ public class DependencyAnalyticsTest {
                 .statusCode(422)
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(equalTo("Unsupported provider: unknown"));
+
+        verifyNoInteractions();
     }
 
     @Test
@@ -70,14 +85,18 @@ public class DependencyAnalyticsTest {
                 .statusCode(422)
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(equalTo("Unsupported package manager: unknown"));
+
+        verifyNoInteractions();
     }
 
     @Test
     public void testComponentAnalysisWithSnyk() {
+        stubSnykRequest(null);
         List<PackageRef> pkgs = List.of(
             new PackageRef("log4j:log4j", "1.2.17"),
             new PackageRef("io.netty:netty-common", "4.1.86"));
         ComponentRequest req = new ComponentRequest(Constants.MAVEN_PKG_MANAGER, Constants.SNYK_PROVIDER, pkgs);
+        
         String body = given()
             .header(CONTENT_TYPE, MediaType.APPLICATION_JSON)
             .body(req)
@@ -89,14 +108,18 @@ public class DependencyAnalyticsTest {
             .extract().body().asPrettyString();
 
         assertJson("snyk_report.json", body);
+        verifyNoInteractionsWithRedHat();
+        verifySnykRequest(null);
     }
 
     @Test
     public void testComponentAnalysisWithRedHat() {
+        stubRedHatRequest();
         List<PackageRef> pkgs = List.of(
             new PackageRef("log4j:log4j", "1.2.17"),
             new PackageRef("io.netty:netty-common", "4.1.86"));
         ComponentRequest req = new ComponentRequest(Constants.MAVEN_PKG_MANAGER, Constants.REDHAT_PROVIDER, pkgs);
+        
         String body = given()
             .header(CONTENT_TYPE, MediaType.APPLICATION_JSON)
             .body(req)
@@ -108,6 +131,34 @@ public class DependencyAnalyticsTest {
             .extract().body().asPrettyString();
 
         assertJson("trustedContent_report.json", body);
+        verifyNoInteractionsWithSnyk();
+        verifyRedHatRequest();
+    }
+
+    @Test
+    public void testSnykClientToken() {
+        String token = "client-token";
+        stubSnykRequest(token);
+       
+        List<PackageRef> pkgs = List.of(
+            new PackageRef("log4j:log4j", "1.2.17"),
+            new PackageRef("io.netty:netty-common", "4.1.86"));
+        ComponentRequest req = new ComponentRequest(Constants.MAVEN_PKG_MANAGER, Constants.SNYK_PROVIDER, pkgs);
+        
+        String body = given()
+            .header(CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(Constants.SNYK_TOKEN_HEADER, token)
+            .body(req)
+        .when()
+            .post("/api/v3/component-analysis")
+        .then()
+            .assertThat()
+            .statusCode(200)
+            .extract().body().asPrettyString();
+
+        assertJson("snyk_report.json", body);
+        verifyNoInteractionsWithRedHat();
+        verifySnykRequest(token);
     }
 
     @Test
@@ -122,6 +173,8 @@ public class DependencyAnalyticsTest {
                 .statusCode(422)
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(equalTo("Unsupported provider: unknown"));
+
+        verifyNoInteractions();
     }
 
     @Test
@@ -136,10 +189,13 @@ public class DependencyAnalyticsTest {
                 .statusCode(422)
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(equalTo("Unsupported package manager: unknown"));
+        
+        verifyNoInteractions();
     }
 
     @Test
     public void testDepAnalysisWithSnyk() {
+        stubSnykRequest(null);
         String body = given()
             .header(CONTENT_TYPE, Constants.TEXT_VND_GRAPHVIZ)
             .body(loadDependenciesFile())
@@ -152,10 +208,13 @@ public class DependencyAnalyticsTest {
                 .extract().body().asPrettyString();
 
         assertJson("snyk_report.json", body);
+        verifyNoInteractionsWithRedHat();
+        verifySnykRequest(null);
     }
 
     @Test
     public void testDepAnalysisWithRedHat() {
+        stubRedHatRequest();
         String body = given()
             .header(CONTENT_TYPE, Constants.TEXT_VND_GRAPHVIZ)
             .body(loadDependenciesFile())
@@ -168,10 +227,15 @@ public class DependencyAnalyticsTest {
                 .extract().body().asPrettyString();
 
         assertJson("trustedContent_report.json", body);
+        verifyNoInteractionsWithSnyk();
+        verifyRedHatRequest();
     }
 
     @Test
     public void testFullDepAnalysisJson() {
+        stubSnykRequest(null);
+        stubRedHatRequest();
+
         String body = given().header(CONTENT_TYPE, Constants.TEXT_VND_GRAPHVIZ).body(loadDependenciesFile())
         .header("Accept", MediaType.APPLICATION_JSON)
             .when()
@@ -182,37 +246,50 @@ public class DependencyAnalyticsTest {
                     .contentType(MediaType.APPLICATION_JSON)
                     .extract().body().asPrettyString();
         assertJson("full_report.json", body);
+
+        verifySnykRequest(null);
+        verifyRedHatRequest();
     }
 
 
     @Test
     public void testFullDepAnalysisHtml() {
-        
-       String body = given().header(CONTENT_TYPE, Constants.TEXT_VND_GRAPHVIZ).body(loadDependenciesFile())
-        .header("Accept", MediaType.TEXT_HTML)
+        stubSnykRequest(null);
+        stubRedHatRequest();
+
+        String body = given().header(CONTENT_TYPE, Constants.TEXT_VND_GRAPHVIZ).body(loadDependenciesFile())
+            .header("Accept", MediaType.TEXT_HTML)
             .when()
-            .post("/api/v3/dependency-analysis")
+                .post("/api/v3/dependency-analysis")
             .then()
                 .assertThat()
                     .statusCode(200)
                     .contentType(MediaType.TEXT_HTML)
                     .extract().body().asString();
+        
         XmlPath p = new XmlPath(CompatibilityMode.HTML, body);
         assertEquals("CRDA Stack Report", p.getString("html.header.title"));
         assertEquals("Snyk", p.getString("html.body.div.find {it.@name == 'snyk_report'}.h2"));
         assertEquals("Red Hat",p.getString("html.body.div.find {it.@name == 'redhat_report'}.h2"));
+        
+        verifySnykRequest(null);
+        verifyRedHatRequest();
     }
 
     @Test
     public void testFullDepAnalysisUnknownMediaType() {
-        given().header(CONTENT_TYPE, Constants.TEXT_VND_GRAPHVIZ).body(loadDependenciesFile())
-        .header("Accept", MediaType.APPLICATION_XML)
+        given()
+            .header(CONTENT_TYPE, Constants.TEXT_VND_GRAPHVIZ)
+            .body(loadDependenciesFile())
+            .header("Accept", MediaType.APPLICATION_XML)
             .when()
-            .post("/api/v3/dependency-analysis")
+                .post("/api/v3/dependency-analysis")
             .then()
                 .assertThat()
                     .statusCode(415)
                     .contentType(MediaType.TEXT_PLAIN);
+        
+        verifyNoInteractions();
     }
 
     private void assertJson(String expectedFile, String currentBody) {
@@ -229,6 +306,52 @@ public class DependencyAnalyticsTest {
 
     private File loadDependenciesFile() {
         return new File(getClass().getClassLoader().getResource("dependencies.txt").getPath());
+    }
+
+    private void stubSnykRequest(String token) {
+        if(token == null) {
+            token = WiremockV3Extension.SNYK_TOKEN;
+        }
+        server.stubFor(post(Constants.SNYK_DEP_GRAPH_API_PATH)
+                .withHeader("Authorization", WireMock.equalTo("token " + token))
+                .withHeader(Exchange.CONTENT_TYPE, WireMock.equalTo(MediaType.APPLICATION_JSON))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBodyFile("snyk_report.json")));
+    }
+
+    private void verifySnykRequest(String token) {
+        if(token == null) {
+            token = WiremockV3Extension.SNYK_TOKEN;
+        }
+        server.verify(1, postRequestedFor(urlEqualTo(Constants.SNYK_DEP_GRAPH_API_PATH))
+            .withHeader("Authorization", WireMock.equalTo("token " + token)));
+    }
+
+    private void stubRedHatRequest() {
+        server.stubFor(post(urlMatching(Constants.TRUSTED_CONTENT_PATH + ".*"))
+            .withHeader(Exchange.CONTENT_TYPE, WireMock.equalTo(MediaType.APPLICATION_JSON))
+            .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader(Exchange.CONTENT_ENCODING, "identity")
+                    .withBodyFile("trustedContent_report.json")));
+    }
+
+    private void verifyRedHatRequest() {
+        server.verify(1, postRequestedFor(urlMatching(Constants.TRUSTED_CONTENT_PATH + ".*")));
+    }
+
+    private void verifyNoInteractions() {
+        verifyNoInteractionsWithSnyk();
+        verifyNoInteractionsWithRedHat();
+    }
+
+    private void verifyNoInteractionsWithSnyk() {
+        server.verify(0, postRequestedFor(urlEqualTo(Constants.SNYK_DEP_GRAPH_API_PATH)));
+    }
+
+    private void verifyNoInteractionsWithRedHat() {
+        server.verify(0, postRequestedFor(urlMatching(Constants.TRUSTED_CONTENT_PATH + ".*")));
     }
 
 }
