ee6fcbec672129a31498158d82da39c8d2778b32
Joachim Schuler <jschuler@redhat.com>
2023-07-31 22:12:09 -0400

    example

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,10 @@
-import '@patternfly/react-core/dist/styles/base.css';
-import { Button } from "@patternfly/react-core";
-import { TableBasic } from './Table';
+import "@patternfly/react-core/dist/styles/base.css";
+import { CompoundExpandable } from "./Table";
 
 function App() {
   return (
     <>
-      <Button onClick={() => alert('Test')}>Test</Button>
-      <div>Woohoo!</div>
-      <div>Total Vulnerabilities: $&#123;body.report.getSummary().getVulnerabilities().getTotal()&#125;</div>
-      <TableBasic />
+      <CompoundExpandable />
     </>
   );
 }
