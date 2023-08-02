2182284b45aaa9c222ac218f29c4839c8e2b7f92
Joachim Schuler <jschuler@redhat.com>
2023-08-01 20:13:51 -0400

    inline assets

--- ui/craco.config.js
+++ ui/craco.config.js
@@ -1,28 +1,38 @@
 module.exports = {
-    webpack: {
-      configure: {
-        output: {
-          filename: 'static/js/[name].js'
-        },
-        optimization: {
-          runtimeChunk: false,
-          splitChunks: {
-            chunks(chunk) {
-              return false
-            },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        filename: "static/js/[name].js",
+      };
+      webpackConfig.optimization = {
+        ...webpackConfig.optimization,
+        runtimeChunk: false,
+        splitChunks: {
+          ...webpackConfig.optimization.splitChunks,
+          chunks(chunk) {
+            return false;
           },
+        },
+      };
+
+      webpackConfig.module.rules[1].oneOf.unshift({
+        test: /\.(png|jpg|jpeg|woff|woff2|eot|ttf|svg)$/,
+        type: "asset/inline",
+      });
+
+      return webpackConfig;
+    },
+  },
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.plugins[5].options.filename = "static/css/[name].css";
+          return webpackConfig;
         },
       },
+      options: {},
     },
-    plugins: [
-      {
-        plugin: {
-          overrideWebpackConfig: ({ webpackConfig }) => {
-            webpackConfig.plugins[5].options.filename = 'static/css/[name].css';
-            return webpackConfig;
-          },
-        },
-        options: {}
-      }
-    ],
-  }
+  ],
+};
