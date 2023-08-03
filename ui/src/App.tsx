5befdb345b9fb74384f0c0bf548a5da5a464055b
Joachim Schuler <jschuler@redhat.com>
2023-08-03 12:36:20 -0400

    tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,61 @@
 import React, { createContext } from "react";
-import { CompoundExpandable } from "./Table";
+import { PageSection, TextContent, Text } from "@patternfly/react-core";
+import { Dashboard } from "./Dashboard";
+import { Command } from "./Command";
+import { MOCK_SBOM } from "./mocks/sbom.mock";
+import { Sbom } from "@app/api/sbom";
 
-export const AppContext = createContext(null);
+const sbom = (window as any)["sbomb"] || MOCK_SBOM;
+
+export const AppContext = createContext<Sbom>(sbom);
 
 function App() {
-  const sbom = (window as any)["sbomb"];
   return (
     <AppContext.Provider value={sbom}>
-      <CompoundExpandable />
+      <Dashboard>
+        <PageSection variant="light">
+          <TextContent>
+            <Text component="h1">React.js + PatternFly 5</Text>
+            <Text component="p">
+              This is a demo to showcase that we can build out a regular CRA
+              application with PatternFly 5, and consume it server-side in the
+              freemarker template.
+            </Text>
+            <Text component="h2">Start mvn server</Text>
+            <Command>
+              mvn quarkus:dev -Dquarkus.http.port=8080 -Dapi.snyk.token=TOKEN
+              -Dapi.trustedContent.vex.host=http://tc-vex-stub-trusted-content.apps.cn-lab2-eu.lue0.p1.openshiftapps.com
+              -Dapi.trustedContent.gav.host=http://swio-trusted-content.apps.cn-lab2-eu.lue0.p1.openshiftapps.com
+            </Command>
+            <Text component="h2">Start UI in dev mode</Text>
+            <Command>
+              {`cd ui
+# first time
+yarn install
+yarn start
+`}
+            </Command>
+            <Text component="h2">
+              Build UI and copy resources to freemarker dir
+            </Text>
+            <Command>yarn build</Command>
+            <Text component="h2">Get a HTML report</Text>
+            <Command>
+              {`http :8080/api/v3/analysis Content-Type:"application/vnd.cyclonedx+json" Accept:"text/html" @'target/project-bom.json' ex-snyk-token:TOKEN ex-oss-index-user:user@redhat.com 'ex-oss-index-token:TOKEN' > report.html`}
+            </Command>
+            <Text component="h2">Access sbom data (example)</Text>
+            <Command>
+              {`const sbom = React.useContext(AppContext);
+console.log(sbom.report.summary.vulnerabilities.total)`}
+            </Command>
+            <Text component="h3">Result:</Text>
+            <div>
+              Total Vulnerabilities:{" "}
+              {sbom && sbom.report.summary.vulnerabilities.total}
+            </div>
+          </TextContent>
+        </PageSection>
+      </Dashboard>
     </AppContext.Provider>
   );
 }
---
+++ ui/src/api/sbom.ts
@@ -0,0 +1,136 @@
+export interface Sbom {
+  packagePath: string;
+  remediationPath: string;
+  issueVisibilityHelper: {
+    providerData?: string;
+  };
+  vexPath: string;
+  report: {
+    summary: {
+      dependencies: {
+        scanned: number;
+        transitive: number;
+      };
+      vulnerabilities: {
+        direct: number;
+        total: number;
+        critical: number;
+        high: number;
+        medium: number;
+        low: number;
+      };
+      providerStatuses: [
+        {
+          ok: boolean;
+          provider: string;
+          status: number;
+          message: string;
+        },
+        {
+          ok: boolean;
+          provider: string;
+          status: number;
+          message: string;
+        }
+      ];
+    };
+    dependencies: Dependency[];
+  };
+  ossIndexIssueLinkFormatter: {
+    issuePathRegex: string;
+  };
+  snykIssueLinkFormatter: {
+    issuePathRegex: string;
+  };
+  sbomPath: string;
+  snykSignup: string;
+  dependencyHelper: {};
+}
+
+export interface Dependency {
+  ref: string;
+  issues: string[];
+  transitive: [
+    {
+      ref: string;
+      issues: {
+        id: string;
+        title: string;
+        source: string;
+        cvss: {
+          attackVector: string;
+          attackComplexity: string;
+          privilegesRequired: string;
+          userInteraction: string;
+          scope: string;
+          confidentialityImpact: string;
+          integrityImpact: string;
+          availabilityImpact: string;
+          exploitCodeMaturity?: string;
+          remediationLevel?: string;
+          reportConfidence?: string;
+          cvss: string;
+        };
+        cvssScore: number;
+        severity: string;
+        cves: string[];
+        unique: boolean;
+      }[];
+      remediations: {
+        [key: string]: {
+          issueRef: string;
+          mavenPackage: string;
+          productStatus: string;
+        };
+      };
+      highestVulnerability: {
+        id: string;
+        title: string;
+        source: string;
+        cvss: {
+          attackVector: string;
+          attackComplexity: string;
+          privilegesRequired: string;
+          userInteraction: string;
+          scope: string;
+          confidentialityImpact: string;
+          integrityImpact: string;
+          availabilityImpact: string;
+          exploitCodeMaturity?: string;
+          remediationLevel?: string;
+          reportConfidence?: string;
+          cvss: string;
+        };
+        cvssScore: number;
+        severity: string;
+        cves: string[];
+        unique: boolean;
+      };
+    }
+  ];
+  recommendation?: string;
+  remediations: {};
+  highestVulnerability: {
+    id: string;
+    title: string;
+    source: string;
+    cvss: {
+      attackVector: string;
+      attackComplexity: string;
+      privilegesRequired: string;
+      userInteraction: string;
+      scope: string;
+      confidentialityImpact: string;
+      integrityImpact: string;
+      availabilityImpact: string;
+      exploitCodeMaturity?: string;
+      remediationLevel?: string;
+      reportConfidence?: string;
+      cvss: string;
+    };
+    cvssScore: number;
+    severity: string;
+    cves: string[];
+    unique: boolean;
+  };
+}
