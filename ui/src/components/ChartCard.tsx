aa886074e32648fd5654b03a715c00f67e7eddda
Olga Lavtar <olavtar@redhat.com>
2023-10-04 12:02:55 -0700

    Changed Summary look.
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- ui/src/components/ChartCard.tsx
+++ ui/src/components/ChartCard.tsx
@@ -1,36 +1,41 @@
 import React from 'react';
 import { Bullseye, Card, CardBody, CardHeader, CardTitle, Divider } from '@patternfly/react-core';
 import { ChartDonut, ChartThemeColor } from '@patternfly/react-charts';
-import {Provider} from '../api/report';
+import { Provider } from '../api/report';
+
+const customColors = ['#800000', '#FF0000', '#FFA500', '#808000'];
 
 export const ChartCard = ({ provider }: { provider: Provider }) => {
-    const customColors = ['#800000', '#FF0000', '#FFA500', '#808000'];
-    return (
-    // <Card isFlat isFullHeight>
-    //   <CardHeader>
-    //     <CardTitle>{`${provider.summary.vulnerabilities.total} vulnerabilities in ${provider.dependencies.length} dependencies`}</CardTitle>
-    //   </CardHeader>
-    //   <Divider />
-    //   <CardBody>
-      <div>
-          <span>{provider.dependencies.length} dependencies</span>
-          <Bullseye>
+
+  const legendData = [
+    { name: `Critical: ${provider.summary.vulnerabilities.critical}`,symbol: { type: 'square', fill: customColors[0] } },
+    { name: `High: ${provider.summary.vulnerabilities.high}`, symbol: { type: 'square', fill: customColors[1] } },
+    { name: `Medium: ${provider.summary.vulnerabilities.medium}`, symbol: { type: 'square', fill: customColors[2] } },
+    { name: `Low: ${provider.summary.vulnerabilities.low}`, symbol: { type: 'square', fill: customColors[3] } },
+  ];
+
+  return (
+    <div>
+      {/* CardHeader */}
+      <CardHeader>
+        <CardTitle>{`${provider.summary.vulnerabilities.total} vulnerabilities in ${provider.dependencies.length} dependencies`}</CardTitle>
+      </CardHeader>
+      <Divider />
+
+      {/* Chart */}
+      <CardBody style={{paddingBottom: "inherit"}}>
+        <Bullseye>
           <div style={{ height: '230px', width: '350px' }}>
             <ChartDonut
               constrainToVisibleArea
               data={[
-                  { x: 'Critical', y: provider.summary.vulnerabilities.critical },
-                  { x: 'High', y: provider.summary.vulnerabilities.high },
-                  { x: 'Medium', y: provider.summary.vulnerabilities.medium,},
-                  { x: 'Low', y: provider.summary.vulnerabilities.low },
+                { x: 'Critical', y: provider.summary.vulnerabilities.critical },
+                { x: 'High', y: provider.summary.vulnerabilities.high },
+                { x: 'Medium', y: provider.summary.vulnerabilities.medium },
+                { x: 'Low', y: provider.summary.vulnerabilities.low },
               ]}
               labels={({ datum }) => `${datum.x}: ${datum.y}%`}
-              legendData={[
-                  { name: `Critical: ${provider.summary.vulnerabilities.critical}` },
-                  { name: `High: ${provider.summary.vulnerabilities.high}` },
-                  {name: `Medium: ${provider.summary.vulnerabilities.medium}`},
-                  { name: `Low: ${provider.summary.vulnerabilities.low}` },
-              ]}
+              legendData={legendData}
               legendOrientation="vertical"
               legendPosition="right"
               padding={{
@@ -46,8 +51,7 @@ export const ChartCard = ({ provider }: { provider: Provider }) => {
             />
           </div>
         </Bullseye>
-      </div>
-    //   </CardBody>
-    // </Card>
+      </CardBody>
+    </div>
   );
 };
