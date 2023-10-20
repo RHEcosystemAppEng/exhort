ef973526e6bfc41b51c4553a375701a11cdfd6e6
Olga Lavtar <olavtar@redhat.com>
2023-10-20 11:29:28 -0700

    Created a common component for showing the rows for Direct and Transitive tables.
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- ui/src/components/TransitiveDependenciesTable.tsx
+++ ui/src/components/TransitiveDependenciesTable.tsx
@@ -1,16 +1,9 @@
-import {Table, TableVariant, Tbody, Td, Th, Thead, Tr} from '@patternfly/react-table';
-
-import {Dependency, TransitiveDependency, buildVulnerabilityItems, hasRemediations} from '../api/report';
-
+import {Table, TableVariant, Tbody, Th, Thead, Tr} from '@patternfly/react-table';
+import {buildVulnerabilityItems, Dependency, TransitiveDependency} from '../api/report';
 import {ConditionalTableBody} from './TableControls/ConditionalTableBody';
 import {Card,} from '@patternfly/react-core';
-import { usePrivateIssueHelper } from '../hooks/usePrivateDataHelper';
-import { SYNK_SIGNUP_URL } from '../utils/utils';
-import { VulnerabilitySeverityLabel } from './VulnerabilitySeverityLabel';
-import { VulnerabilityScore } from './VulnerabilityScore';
-import { DependencyLink } from './DependencyLink';
-import { RemediationLink } from './RemediationLink';
-import { VulnerabilityLink } from './VulnerabilityLink';
+import {usePrivateIssueHelper} from '../hooks/usePrivateDataHelper';
+import {VulnerabilityRow} from "./VulnerabilityRow";
 
 export const TransitiveDependenciesTable = ({
                                               providerName,
@@ -30,56 +23,17 @@ export const TransitiveDependenciesTable = ({
             <Th width={10}>Vulnerability ID</Th>
             <Th width={25}>Description</Th>
             <Th width={10}>Severity</Th>
-            <Th width={15}>CVSS Score</Th>
+            <Th width={20}>CVSS Score</Th>
             <Th width={20}>Transitive Dependency</Th>
             <Th>Remediation</Th>
           </Tr>
         </Thead>
         <ConditionalTableBody isNoData={transitiveDependencies.length === 0} numRenderedColumns={7}>
-        {buildVulnerabilityItems(transitiveDependencies).map((item, rowIndex) => {
-            return (
-              <Tbody key={rowIndex}>
-                  
-                <Tr key={rowIndex}>
-                  {privateIssueHelper.hideIssue(providerName, item.vulnerability.unique) ? (
-                    <>
-                      <Td colSpan={3}>
-                        <a href={SYNK_SIGNUP_URL} target="_blank" rel="noreferrer">
-                          Sign up for a Snyk account
-                        </a>{' '}
-                        to learn about the vulnerabilities found
-                      </Td>
-                    </>
-                  ) : (
-                    <>
-                      <Td>
-                        <p>{item.id}</p>
-                      </Td>
-                      <Td>{item.vulnerability.title}</Td>
-                      <Td noPadding>
-                        <VulnerabilitySeverityLabel vulnerability={item.vulnerability}/>
-                      </Td>
-                    </>
-                  )}
-                  <Td>
-                    <VulnerabilityScore vulnerability={item.vulnerability}/>
-                  </Td>
-                  <Td>
-                    <DependencyLink name={item.dependencyRef}/>
-                  </Td>
-                  <Td>
-                  {item.vulnerability.remediation?.trustedContent && 
-                        <RemediationLink key={rowIndex} cves={item.vulnerability.cves || []} packageName={item.dependencyRef} />
-                    }
-                    {item.vulnerability.remediation?.fixedIn && 
-                        <VulnerabilityLink sourceName={providerName} vulnerability={item.vulnerability} />
-                    }
-                    {!hasRemediations(item.vulnerability) && "N/A"}
-                  </Td>
-                </Tr>
-              </Tbody>
-            )
-          })}
+          {buildVulnerabilityItems(transitiveDependencies).map((item, rowIndex) => (
+            <Tbody key={rowIndex}>
+              <VulnerabilityRow item={item} providerName={providerName} rowIndex={rowIndex} />
+            </Tbody>
+          ))}
         </ConditionalTableBody>
       </Table>
     </Card>
